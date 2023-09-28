4f6d135ce1299d861b0f39449da7d6246846f60a
Charlotte Divine <divinemaina@gmail.com>
2023-09-28 17:33:31 +0200

    solve excercesise1

---
+++ react-projects/src/components/Exercesise1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Exercesise1 = () => {
+  return (
+    <div id="Exercesise1" className="w-full h-screen flex justify-center items-center  bg-yellow-500">
+      <h1 className="text-center">Exercesise1</h1>
+    </div>
+  );
+};
+
+export default Exercesise1;
