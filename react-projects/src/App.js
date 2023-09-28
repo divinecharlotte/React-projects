f8299177fa508a14b7ade1b3d9a830ce8f5de37d
Charlotte Divine <divinemaina@gmail.com>
2023-09-28 17:49:20 +0200

    solve excercesise2

--- react-projects/src/App.js
+++ react-projects/src/App.js
@@ -1,9 +1,10 @@
 import Exercesise1 from "./components/Exercesise1";
+import Exercesise2 from "./components/Exercesise2";
 
 function App() {
   return <div className="App">
-
     <Exercesise1/>
+    <Exercesise2/>
   </div>;
 }
 
