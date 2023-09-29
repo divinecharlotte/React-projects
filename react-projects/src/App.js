6fa9d96e82131e94f91e028e75605163086c7397
Charlotte Divine <divinemaina@gmail.com>
2023-09-29 15:05:09 +0200

    solve exercesise4

--- react-projects/src/App.js
+++ react-projects/src/App.js
@@ -1,12 +1,14 @@
 import Exercesise1 from "./components/Exercesise1";
 import Exercesise2 from "./components/Exercesise2";
 import Exercesise3 from "./components/Exercesise3";
+import Exercesise4 from "./components/Exercesise4";
 
 function App() {
   return <div className="App">
     <Exercesise1/>
     <Exercesise2 id=""/>
     <Exercesise3/>
+    <Exercesise4/>
   </div>;
 }
 
