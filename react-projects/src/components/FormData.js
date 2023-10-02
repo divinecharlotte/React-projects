bba6abcc250dc032fb330bb4bde0eafc4be9f3c9
Charlotte Divine <divinemaina@gmail.com>
2023-10-02 16:03:01 +0200

    solve exercesise7

--- react-projects/src/components/FormData.js
+++ react-projects/src/components/FormData.js
@@ -42,7 +42,7 @@ import {React,useState} from 'react'
         <button onClick={handleclick}
         className='bg-red-700 text-white px-16 py-2 '
          >
-            GREET ME
+            GREET me
             </button>
         </form> 
     </div>
